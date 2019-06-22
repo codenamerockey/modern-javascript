4fe120e7951b77063344379e78f7bc885262f324
Rockey Simmons Jr <37706223+codenamerockey@users.noreply.github.com>
2019-06-22 06:52:37 -0700

    Added sort method to sort an array of numbers.

--- chapter_9/sandbox.js
+++ chapter_9/sandbox.js
@@ -12,6 +12,13 @@ const scores = [10, 50, 20, 5, 35, 70, 45];
 scores.reverse();
 console.log(scores);
 
+
+// Better way to sort an array of numbers
+scores.sort((a, b) => b - a); // sorts numbers highest to lowest
+scores.sort((a, b) => a - b); // sorts numbers lowest to highest 
+console.log(scores);
+console.log(scores.reverse()); // also takes sorted numbers and reverses the order vice versa.
+
 // example 3 - sorting objects
 const players = [
   {name: 'mario', score: 20},
